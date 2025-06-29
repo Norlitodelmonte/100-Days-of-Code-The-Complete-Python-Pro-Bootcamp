5929532620a66aaadbdd34f527e7a9f293683588
Norlitodelmonte <norlitodelmonte2603@gmail.com>
2025-06-29 18:32:21 +0800

    Update and rename main.py to Band_namer_generator.py

---
+++ day01/Band_namer_generator.py
@@ -0,0 +1,4 @@
+print("Welcome sa Combine Name generator")
+name = input("Ano ang pangalan mo?\n")
+pet = input("Ano ang panagalan ng alaga mo?\n")
+print = input("Ang combine name mo ay:" + name + " " + pet )
