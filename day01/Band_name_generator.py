d400a6797934444464fc98e824a006859e3c67ca
Norlitodelmonte <norlitodelmonte2603@gmail.com>
2025-07-01 15:10:45 +0800

    Update Band_name_generator.py

--- day01/Band_name_generator.py
+++ day01/Band_name_generator.py
@@ -1,4 +1,4 @@
-print("Welcome sa Combine Name generator")
-name = input("Ano ang pangalan mo?\n")
-pet = input("Ano ang panagalan ng alaga mo?\n")
-print = input("Ang combine name mo ay:" + name + " " + pet )
+print("Welcome to the Band Name Generator")    
+city = input("What's the name of the city you greaw up in?")
+pet = input("What's your pets name?")
+print("Your band name could be:" + city + " " + pet)
